5d94a1a24d9e5a55770a66c30677598e5252d384
Thomas Marcarello <thomas@marcarello.tech>
2025-10-15 17:46:49 -0400

    Added logging for function and logging function.

--- Detect-Ninja.ps1
+++ Detect-Ninja.ps1
@@ -16,8 +16,26 @@ $InstallLogPath = $env:TEMP
 $InstallLogFile = "$InstallLogPath\ninjaone.log"
 
 $NinjaURL = Get-SyncroSelection -Client "NCSC" #Remove this line and replace with switch case that this function performs.
+$Override = $false
 
-#Write-Host $NinjaURL
+function Write-Log {
+    param (
+        [switch]$Info,
+        [switch]$Success,
+        [switch]$Fail,
+        [string]$Message
+    )
+    
+    if($Info.IsPresent){
+        Write-Host "[INFO] $Message"
+    }
+    if($Success.IsPresent){
+        Write-Host "[SUCCESS] $Message"
+    }
+    if($Fail.IsPresent){
+        Write-Host "[FAIL] $Message"
+    }
+}
 
 #Detection function
 function Get-NinjaAgentInstall {
@@ -30,26 +48,45 @@ function Get-NinjaAgentInstall {
     $Target = "$Programpath\$Agent"
 
     #Checking WMI for ninja install instance, in a botched install instance, this may detect
-    if($null -ne $(Get-WmiObject -Class Win32_Product | where-object {$_.Name -like "NinjaRMMAgent"})){
+    Write-Log -Info -Message "Checking WMI..."
+    if($null -ne $(Get-WmiObject -Class Win32_Product -ErrorAction SilentlyContinue | where-object {$_.Name -like "NinjaRMMAgent"})){
+        Write-Log -Success -Message "Found NinjaRMMAgent in WMI querry."
         $check1 = $true
+    }
+    else {
+        Write-Log -Fail -Message "NinjaRMMAgent NOT found in WMI querry."
     }
 
     #Checking for file path
+    Write-Log -Info -Message "Checking file path..."
     if(Test-Path -Path $ProgramPath){
+        Write-Log -Success -Message "Folder found."
         #Checking to make sure the RMM agent
-        If(Test-Path -Path $Target){
+        if(Test-Path -Path $Target){
+            Write-Log -Success -Message "Agent exe found."
             $Check2 = $true
         }
+        else {
+            Write-Log -Fail -Message "EXE NOT found."
+        }
+    }
+    else {
+        Write-Log -Fail -Message "File path and exe NOT found."
     }
 
     #Checking for service and if service can be ran or is running
+    Write-Log -Info -Message "Checking service running..."
     if($null -ne $(Get-Service -Name "NinjaRMMAgent" -ErrorAction SilentlyContinue)){
+        Write-Log -Success -Message "NinjaRMMAgent service found."
         if($(Get-Service -Name "NinjaRMMAgent").Status -eq "Running"){
+            Write-Log -Success -Message "NinjaRMMAgent service is running."
             $Check3 = $true
+        }
+        else {
+            Write-Log -Fail -Message "NinjaRMMAgent service is NOT running."
         }
     }
 
-    return $Check1 -and $Check2 -and $Check3
+    return ($Check1 -or $Check2) -and $Check3
 }
 
-Write-Host $(Get-NinjaAgentInstall)
